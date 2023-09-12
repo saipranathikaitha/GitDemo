f6ccc36dbd9f3994892d691e5b0d8b57a7ee4bff
sony <saipranathikaitha@gmail.com>
2023-09-12 05:25:28 -0700

    first commit

---
+++ src/sel1.java
@@ -0,0 +1,15 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class sel1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.setProperty("Webdriver.chrome.driver","/Documents/chromedriver.exe");
+            WebDriver driver= new ChromeDriver();
+            driver.get("https://rahulshettyacademy.com");
+            System.out.println(driver.getTitle());
+            System.out.println(driver.getCurrentUrl());
+	}
+
+}
